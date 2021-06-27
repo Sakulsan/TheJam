a00e16b53fdb33532cb4a6991baf08a6dd012ffc
Bearryy <maya.leeloo@gmail.com>
2021-06-28 00:46:37 +0200

    snövärld bg 0,1

--- Entity.cs
+++ Entity.cs
@@ -11,9 +11,6 @@ namespace TheJam
     {
         public int x;
         public int y;
-        decimal frameLength;
-        int currentFrame = 0;
-        int millisLastFrame = 0;
         public int depth;
         public Rectangle drawPosition;
         public Texture2D sprite;
@@ -30,42 +27,16 @@ namespace TheJam
             this.sprite = sprite;
             this.game = game;
             drawPosition = new Rectangle(x * game.scale, y * game.scale, game.scale, game.scale);
-            frameLength = 0;
-        }
-
-        public Entity(int x, int y, int framerate, int depth,  bool collision, Texture2D sprite, Game1 game)
-        {
-            this.x = x;
-            this.y = y;
-            this.depth = depth;
-            this.collision = collision;
-            this.sprite = sprite;
-            this.game = game;
-            this.frameLength = 1000 / ((decimal)framerate);
-            drawPosition = new Rectangle(x * game.scale, y * game.scale, game.scale, game.scale);
         }
 
         public virtual void Update(GameTime gt, List<Entity> enties)
         {
-            if (frameLength != 0)
-            {
-                millisLastFrame += (int)gt.ElapsedGameTime.TotalMilliseconds;
-
-                //int movedFrames = (int)Math.Floor(((decimal)millisLastFrame) / (decimal)frameLength);
-                if (millisLastFrame > frameLength)
-                {
-                    currentFrame++;
-                    int frameCount = sprite.Height / 16;
-                    currentFrame %= frameCount;
-                    millisLastFrame = 0;
-                }
-            }
             drawPosition = new Rectangle(x * game.scale, y * game.scale, game.scale, game.scale);
         }
 
         public void Draw(SpriteBatch sb)
         {
-            sb.Draw(sprite, drawPosition , new Rectangle(0,currentFrame * 16,16,16) , Color.White, rotation: 0f, origin: Vector2.Zero, effects: SpriteEffects.None,layerDepth: (depth + 5) / 10);
+            sb.Draw(sprite, drawPosition , new Rectangle(0,0,sprite.Width,sprite.Height) , Color.White, rotation: 0f, origin: Vector2.Zero, effects: SpriteEffects.None,layerDepth: (depth + 5) / 10);
     }
     }
 
--- Game1.cs
+++ Game1.cs
@@ -22,10 +22,8 @@ namespace TheJam
         public int scale = 128;
         public int offset = 0;
 
-        public string debug = "";
-
         public Map[,] currentZone = new Map[3,3];
-        public Point zoneCoordinates = new Point(1, 1);
+        public Point zoneCoordinates = new Point(1, 2);
 
         public Game1()
         {
@@ -41,7 +39,6 @@ namespace TheJam
 
             _graphics.PreferredBackBufferHeight = 1080;
             _graphics.PreferredBackBufferWidth = 1920;
-            //_graphics.IsFullScreen = true;
             _graphics.ApplyChanges();
 
 
@@ -61,7 +58,7 @@ namespace TheJam
             currentZone = LoadZone1();
 
             Texture2D placeHolderTile = Content.Load<Texture2D>("Placeholder");
-            //currentZone[0, 1].entities.Add(new LeaveTile(5,0,placeHolderTile,false,0,0,, this));
+            currentZone[0, 1].entities.Add(new LeaveTile(5,0,placeHolderTile,false,0,0, this));
 
 
             //Entity e = entities.Find(test => test.x == 2 && test.y == 2);
@@ -79,9 +76,9 @@ namespace TheJam
 
             foreach (Entity e in currentZone[zoneCoordinates.X, zoneCoordinates.Y].entities) e.Update(gameTime, currentZone[zoneCoordinates.X,zoneCoordinates.Y].entities);
 
-            if(Keyboard.GetState().IsKeyDown(Keys.F)) fadeGaol = 255;
-            if (fadeGaol - 17 > fadethrough) fadethrough += gameTime.ElapsedGameTime.Milliseconds;
-            else if (fadeGaol + 17 < fadethrough) fadethrough -= gameTime.ElapsedGameTime.Milliseconds;
+            if(Keyboard.GetState().IsKeyDown(Keys.F)) fadeGaol = 255 * 5;
+            if (fadeGaol - 20 > fadethrough) fadethrough += gameTime.ElapsedGameTime.Milliseconds;
+            else if (fadeGaol + 20 < fadethrough) fadethrough -= gameTime.ElapsedGameTime.Milliseconds;
             else if (fadethrough != 0) fadeGaol= 0;
             // TODO: Add your update logic here
 
@@ -112,9 +109,8 @@ namespace TheJam
             }
 
             foreach (Entity e in currentZone[zoneCoordinates.X,zoneCoordinates.Y].entities) e.Draw(_spriteBatch);
-            //_spriteBatch.DrawString(arial, $"{fadethrough} : {fadeGaol}", Vector2.Zero, Color.Black);
-            _spriteBatch.DrawString(arial, debug, Vector2.Zero, Color.Black);
-            if (fadethrough != 0) _spriteBatch.Draw(pixel, new Vector2(0, 0), new Rectangle(0, 0, 1, 1), new Color(0, 0, 0, fadethrough), 0f, Vector2.Zero, 1920, SpriteEffects.None, 0.6f);
+            _spriteBatch.DrawString(arial, $"{fadethrough} : {fadeGaol}", Vector2.Zero, Color.Black);
+            if (fadethrough != 0) _spriteBatch.Draw(pixel, new Vector2(32, 32), new Rectangle(0, 0, 1, 1), new Color(0, 0, 0, fadethrough  / 5 ), 0f, Vector2.Zero, 600, SpriteEffects.None, 0.6f);
             
 
             _spriteBatch.End();
@@ -141,8 +137,8 @@ namespace TheJam
                 for (int j = 0; j < 3; j++)
                 {
                     Map generating = new Map(new List<Entity>(), Content.Load<Texture2D>("Placeholder"));
-                    generating.entities.Add(new Player(1, 1, 10,Content.Load<Texture2D>(@"Main pig\Forward"), this));
-                    //generating.entities.Add(new TouchEntity(4, 0, 5, true, Content.Load<Texture2D>("Button"), TouchEntity.Effects.Textbox, "Hello world", this));
+                    generating.entities.Add(new Player(1, 1, Content.Load<Texture2D>("piggo"), this));
+                    generating.entities.Add(new TouchEntity(4, 0, 5, true, Content.Load<Texture2D>("Button"), TouchEntity.Effects.Textbox, "Hello world", this));
                     
                     for (int u = 0; u < 9; u++) generating.entities.Add(new Entity(-1, u, 0, true, nothing, this));
                     for (int u = 0; u < 8; u++) generating.entities.Add(new Entity(u, -1, 0, true, nothing, this));
@@ -153,10 +149,7 @@ namespace TheJam
                     v[i, j] = generating;
                 }
             }
-            v[1, 2].background = Content.Load<Texture2D>(@"Estetiska\snövärld bg 1,2");
-            v[1, 2].entities.Add(new LeaveTile(4, 0, Content.Load<Texture2D>("Placeholder"), false, 1, 1, 4, 7, this));
-            v[1, 1].background = Content.Load<Texture2D>(@"Estetiska\snövärld bg 1,1");
-            v[1, 1].entities.Add(new LeaveTile(4, 7, Content.Load<Texture2D>("Placeholder"), false, 1, 2,4,0, this));
+            v[1, 2].background = Content.Load<Texture2D>(@"Estetiska\snövärd bg12");
 
             return v;
         }
